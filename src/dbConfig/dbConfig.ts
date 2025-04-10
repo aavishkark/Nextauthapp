1d7f624287a1fea8e4a28da4c07707854d4f3d4c
Avishkar Kamble <aavishkark@gmail.com>
2025-04-10 16:46:29 +0530

    eting up db config file and connecting to mongodb

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+        connection.on("connected", () => {
+            console.log("Connected to MongoDB");
+        })
+        connection.on("error", (err) => {
+            console.log("Connection to mongodb unsucessful" + err);
+            process.exit();
+        })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
